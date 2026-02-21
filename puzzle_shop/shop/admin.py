4cd7953520f8699690b1a19861d329bf6114b9e6
Amelia <ameliasolder@mail.ru>
2026-02-21 10:03:11 +0300

    Add files via upload

--- puzzle_shop/shop/admin.py
+++ puzzle_shop/shop/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from .models import Category, Manufacturer, Product, Cart, CartItem
 
-# Register your models here.
+# Регистрация модели "Категория"
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['название', 'описание']
+    search_fields = ['название']
+
+
+# Регистрация модели "Производитель"
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = ['название', 'страна']
+    search_fields = ['название', 'страна']
+
+
+# Регистрация модели "Товар"
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['название', 'цена', 'количество_на_складе', 'категория', 'производитель']
+    list_filter = ['категория', 'производитель']
+    search_fields = ['название', 'описание']
+
+
+# Inline для элементов корзины (показываем их внутри корзины)
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 1
+
+
+# Регистрация модели "Корзина"
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['пользователь', 'дата_создания']
+    inlines = [CartItemInline]
+
+
+# Регистрация модели "Элемент корзины"
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['товар', 'корзина', 'количество', 'стоимость_элемента']
--- puzzle_shop/shop/models.py
+++ puzzle_shop/shop/models.py
@@ -1,3 +1,124 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+# Модель "Категория товара"
+class Category(models.Model):
+    название = models.CharField(max_length=100, verbose_name="Название категории")
+    описание = models.TextField(blank=True, verbose_name="Описание")
+    
+    def __str__(self):
+        return self.название
+    
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+# Модель "Производитель"
+class Manufacturer(models.Model):
+    название = models.CharField(max_length=100, verbose_name="Название производителя")
+    страна = models.CharField(max_length=100, verbose_name="Страна")
+    описание = models.TextField(blank=True, verbose_name="Описание")
+    
+    def __str__(self):
+        return self.название
+    
+    class Meta:
+        verbose_name = "Производитель"
+        verbose_name_plural = "Производители"
+
+
+# Модель "Товар"
+class Product(models.Model):
+    название = models.CharField(max_length=200, verbose_name="Название товара")
+    описание = models.TextField(verbose_name="Описание")
+    фото_товара = models.ImageField(upload_to='products/', blank=True, null=True, verbose_name="Фото товара")
+    цена = models.DecimalField(
+        max_digits=10, 
+        decimal_places=2, 
+        validators=[MinValueValidator(0)],
+        verbose_name="Цена"
+    )
+    количество_на_складе = models.IntegerField(
+        validators=[MinValueValidator(0)],
+        verbose_name="Количество на складе"
+    )
+    категория = models.ForeignKey(
+        Category, 
+        on_delete=models.CASCADE,
+        verbose_name="Категория"
+    )
+    производитель = models.ForeignKey(
+        Manufacturer, 
+        on_delete=models.CASCADE,
+        verbose_name="Производитель"
+    )
+    
+    def __str__(self):
+        return self.название
+    
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
+
+# Модель "Корзина"
+class Cart(models.Model):
+    пользователь = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE,
+        verbose_name="Пользователь"
+    )
+    дата_создания = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    
+    def __str__(self):
+        return f"Корзина пользователя {self.пользователь.username}"
+    
+    def общая_стоимость(self):
+        """Вычисляет общую стоимость всех товаров в корзине"""
+        total = 0
+        for item in self.cartitem_set.all():
+            total += item.стоимость_элемента()
+        return total
+    
+    class Meta:
+        verbose_name = "Корзина"
+        verbose_name_plural = "Корзины"
+
+
+# Модель "Элемент корзины"
+class CartItem(models.Model):
+    корзина = models.ForeignKey(
+        Cart, 
+        on_delete=models.CASCADE,
+        verbose_name="Корзина"
+    )
+    товар = models.ForeignKey(
+        Product, 
+        on_delete=models.CASCADE,
+        verbose_name="Товар"
+    )
+    количество = models.PositiveIntegerField(verbose_name="Количество")
+    
+    def __str__(self):
+        return f"{self.товар.название} ({self.количество} шт.)"
+    
+    def стоимость_элемента(self):
+        """Вычисляет стоимость этого элемента корзины"""
+        return self.товар.цена * self.количество
+    
+    def clean(self):
+        """Проверяет, что количество не превышает количество на складе"""
+    from django.core.exceptions import ValidationError
+    
+    # Проверяем что товар и количество установлены
+    if self.товар and self.количество:
+        if self.количество > self.товар.количество_на_складе:
+            raise ValidationError(
+                f'Недостаточно товара на складе. Доступно: {self.товар.количество_на_складе}'
+            )
+    
+    class Meta:
+        verbose_name = "Элемент корзины"
+        verbose_name_plural = "Элементы корзины"
