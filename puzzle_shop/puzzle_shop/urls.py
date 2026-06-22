548f6f34b09cd6b35794a7b1b934def28fb60467
Amelia <ameliasolder@mail.ru>
2026-06-22 22:33:20 +0300

    21

--- puzzle_shop/puzzle_shop/urls.py
+++ puzzle_shop/puzzle_shop/urls.py
@@ -6,9 +6,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('api-auth/', include('rest_framework.urls')),  # Добавили!
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('shop.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
